dd72b6725d7458d6017eab641979e2c1af8a4699
Sean Rowe <seanrowe3@gmail.com>
2022-12-03 15:14:53 -0600

    Days 2 & 3

---
+++ 2022/day2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace AdventOfCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] lines = System.IO.File.ReadAllLines(@"C:\sourcecode\adventofcode\2022\day2\input\input.txt");
+
+            int currentScore = 0;
+            foreach (string line in lines)
+            {
+                if (line.Contains("A"))
+                {
+                    if (line.Contains("X")) currentScore += (0 + 3);
+                    if (line.Contains("Y")) currentScore += (3 + 1);
+                    if (line.Contains("Z")) currentScore += (6 + 2);
+                }
+                else if (line.Contains("B"))
+                {
+                    if (line.Contains("X")) currentScore += (0 + 1);
+                    if (line.Contains("Y")) currentScore += (3 + 2);
+                    if (line.Contains("Z")) currentScore += (6 + 3);
+                }
+                else if (line.Contains("C"))
+                {
+                    if (line.Contains("X")) currentScore += (0 + 2);
+                    if (line.Contains("Y")) currentScore += (3 + 3);
+                    if (line.Contains("Z")) currentScore += (6 + 1);
+                }
+            }
+            Console.WriteLine($"TotalScore: {currentScore}!");
+        }
+    }
+}
+
+// A is Rock, X is lose (0pt), Rock is 1pt
+// B is Paper, Y is draw(3pt), Paper is 2pt
+// C is Scissors, Z is win(6pt), Scissor is 3pt
---
+++ 2022/day3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] lines = System.IO.File.ReadAllLines(@"C:\sourcecode\adventofcode\2022\day3\input\input.txt");
+
+            int prioritySum = 0;
+            for (int i = 0; i < lines.Count(); i += 3)
+            {
+                char common = FindCommonChar(lines[i], lines[i+1], lines[i+2]);
+                int priority = (int)common % 32;
+                if (Char.IsUpper(common)){
+                    priority = priority + 26;
+                }
+                prioritySum += priority;
+            }
+            Console.WriteLine($"PrioritySum: {prioritySum}!");
+        }
+
+        static char FindCommonChar(string s1, string s2, string s3)
+        {
+            foreach(char c in s1){
+                foreach(char d in s2){
+                    foreach(char e in s3){
+                        if (c == d && d == e){
+                            return c;
+                        }
+                    }
+                }
+            }
+            return '\n';
+        }
+    }
+}
